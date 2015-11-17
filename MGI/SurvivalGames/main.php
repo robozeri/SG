ec4bf8381f99c2381d162e19b6c22dc2166935c9
robozeri <robozerirobot@gmail.com>
2015-11-17 20:01:15 +0100

    Update main.php

--- MGI/SurvivalGames/main.php
+++ MGI/SurvivalGames/main.php
@@ -1,1 +1,19 @@
+<?php
+
+namespace MGI;
+
+/*
+Survival Games for the server MGI
+*/
+
+use pocketmine\plugin\PluginBase;
+
+class main {
+  
+  public function onEnable(){
+    
+    
+  }
+  
+}
 
