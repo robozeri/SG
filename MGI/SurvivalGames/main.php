5c02c94897d5a81edde807e425fbfabe831d89d3
robozeri <robozerirobot@gmail.com>
2015-11-17 20:02:09 +0100

    Update main.php

--- MGI/SurvivalGames/main.php
+++ MGI/SurvivalGames/main.php
@@ -7,8 +7,9 @@ Survival Games for the server MGI
 */
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 
-class main {
+class main extends PluginBase implements Listener{
   
   public function onEnable(){
     
